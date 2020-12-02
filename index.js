c2a0daab2c52febc5f5cb003698f8f4003bf329f
Chris Trevino <chrisdadef@gmail.com>
2020-12-01 18:09:51 -0600

    test commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our my freaking new server!')
+  res.send('Would you look at that!')
 })
 
 app.listen(port, () => {
